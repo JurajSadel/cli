e20173eb9169d2ea19c9e3aeafd1c1790275e6f9
JurajSadel <43887390+JurajSadel@users.noreply.github.com>
2021-06-15 12:47:46 +0200

    Resolved issues

--- src/main.rs
+++ src/main.rs
@@ -8,33 +8,32 @@ struct Envelope {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-struct Chip
-{
+struct Chip {
     name: String,
     bluetooth: String,
-    operatingTemp: String,
+    #[serde(rename = "operatingTemp")]
+    operating_temp: String,
     psram: i8
 
 }
 
 //filter by entered chip name as cmd argument
-fn parse_name(name_arg:Vec<&str>, raw:&Envelope )
-{
+fn parse_name(name_arg:Vec<&str>, raw:&Envelope ) {
+
     for name in name_arg {
         print!("\nName {}:\n", name);
-        
+
         let mut unknown_chip = true;
-        for chip in &raw.results { 
-            
-            if chip.name == name 
-            {
+        for chip in &raw.results {
+
+            if chip.name == name {
                 println!("bluetooth: {}", chip.bluetooth);
-                if chip.psram == 0
-                {
+
+                if chip.psram == 0 {
                     println!("psram: Not present");
                 }
                 else {
-                    
+
                     println!("psram: {}", chip.psram);
                 }
 
@@ -43,18 +42,16 @@ fn parse_name(name_arg:Vec<&str>, raw:&Envelope )
             }
         };
 
-    if unknown_chip == true
-    {
+    if unknown_chip == true {
         println!("Assigned unknown name!");
     }
 }
 }
 
 //parse string temperature into integers
-fn parse_string(temperature_str:&str) -> (i16, i16)
-{
+fn parse_string(temperature_str:&str) -> (i16, i16) {
     let v:Vec<&str> = temperature_str.split(|c| c == ' ').collect();
-    
+
     let min_temp = v[0].parse::<i16>().unwrap();
     let mut max_temp = 0;
 
@@ -66,16 +63,13 @@ fn parse_string(temperature_str:&str) -> (i16, i16)
 }
 
 //filter by entered temperature as cmd argument
-fn parse_temperature(temperature_arg:&str, raw:&Envelope )
-{
+fn parse_temperature(temperature_arg:&str, raw:&Envelope ) {
     let pair_arg_temp = parse_string(temperature_arg);
 
     print!("\nTemperature {}:\n[ ",temperature_arg);
-    for chip in &raw.results
-    {
-        let pair_chip_temp = parse_string(&chip.operatingTemp);
-        if pair_arg_temp.0 >= pair_chip_temp.0 && pair_arg_temp.0 <= pair_chip_temp.1
-        {
+    for chip in &raw.results {
+        let pair_chip_temp = parse_string(&chip.operating_temp);
+        if pair_arg_temp.0 >= pair_chip_temp.0 && pair_arg_temp.0 <= pair_chip_temp.1 {
             print!("{} ", chip.name);
         }
     }
@@ -83,7 +77,7 @@ fn parse_temperature(temperature_arg:&str, raw:&Envelope )
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	
+
     let matches = App::new("My Super Program")
                             .arg(Arg::with_name("name")
                                 .short("n")
@@ -99,27 +93,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                                 .help("Operating Temperature"))
                             .get_matches();
 
-    //let name_arg = matches.value_of("name").unwrap_or("");
-    let name_arg:Vec<&str>  = matches.values_of("name").unwrap().collect();
-    //let name_arg  = matches.values_of("name").expect("Chip name missing!");
-
-    let temperature_arg = matches.value_of("operatingTemp").unwrap_or("");
-    
-    //println!("{:?}", name_arg);
-    //println!("{:?}", temperature_arg);
-    
     //get JSON data
     let response = reqwest::blocking::get("https://products.espressif.com/iot-solution-api/query?language=en")?;
     let text = response.text()?;
     let raw: Envelope = serde_json::from_str(&text)?;
-    
-    //let filtered = serde_json::to_string(&raw.results)?;
-    //println!("{}", &filtered);
-    //std::fs::write("data.json", filtered)?;
 
-    parse_name(name_arg, &raw);
-    parse_temperature(temperature_arg, &raw);
+    if matches.is_present("name") {
+        let name_arg:Vec<&str>  = matches.values_of("name").unwrap().collect();
+        parse_name(name_arg, &raw);
+    }
+    else {
+        println!("\nUser entered no name arguments");
+    }
+    if matches.is_present("operatingTemp") {
+        let temperature_arg = matches.value_of("operatingTemp").unwrap_or("");
+        parse_temperature(temperature_arg, &raw);
+    }
+    else {
+        println!("\nUser entered no temperature argument");
+    }
 
 	Ok(())
-
 }
